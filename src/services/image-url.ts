8fd275640c4fc76df2dacd0d5a79dc9d2671f269
Chris Farmer <farmercm1@gmail.com>
2023-08-11 12:39:45 -0400

    Get optimized images

---
+++ src/services/image-url.ts
@@ -0,0 +1,10 @@
+//default: https://media.rawg.io/media/crop/games/456/456dea5e1c7e3cd07060c14e96612001.jpg
+//smaller: https://api.rawg.io/media/crop/600/400/games/736/73619bd336c894d6941d926bfd563946.jpg
+
+const getCroppedImageUrl = (url: string) => {
+  const target = "media/";
+  const index = url.indexOf(target) + target.length;
+  return url.slice(0, index) + "crop/600/400/" + url.slice(index);
+};
+
+export default getCroppedImageUrl;
