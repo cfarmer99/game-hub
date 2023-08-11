80a27a4891b5e7c43336d185e5f1e8bec9e15645
Chris Farmer <farmercm1@gmail.com>
2023-08-11 13:16:51 -0400

    Show loading skeletons

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -23,22 +23,31 @@ interface FetchGamesResponse {
 const useGames = () => {
   const [games, setGames] = useState<Game[]>([]);
   const [error, setError] = useState("");
+  const [isLoading, setLoading] = useState(false);
 
   useEffect(() => {
     const controller = new AbortController();
+    setLoading(true);
     apiClient
       .get<FetchGamesResponse>("/games", { signal: controller.signal })
-      .then((res) => setGames(res.data.results))
+      .then((res) => {
+        setGames(res.data.results);
+        setLoading(false); // have to put this when in strict mode. Finally doesn't work
+      })
       .catch((err) => {
         if (err instanceof CanceledError) return;
         setError(err.message);
+        setLoading(false);
+      })
+      .finally(() => {
+        //setLoading(false);
       });
     return () => controller.abort();
     // must pass empty array as 2nd parameter or this will run continuously!
     // this is because every time the callout is resolved, the state changes, resulting in
     // another callout, resulting in an infinite loop
   }, []);
-  return { games, error };
+  return { games, error, isLoading };
 };
 
 export default useGames;
